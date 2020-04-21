42e0bb3e28766c5bf773ccaab40ab66d50b4b10e
David Martinez Ros <davidnezan@gmail.com>
2020-04-21 18:41:21 +0200

    Auto-generated commit

---
+++ precache-manifest.f00dd5b9c99f1cfe15ed742b20736912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8747491ef856fbc9100477b081d1a46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0db63d0b65eef9758bb8",
+    "url": "/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "58952e0e58ed537362e9",
+    "url": "/static/css/main.b0280506.chunk.css"
+  },
+  {
+    "revision": "0db63d0b65eef9758bb8",
+    "url": "/static/js/2.ae26c137.chunk.js"
+  },
+  {
+    "revision": "58952e0e58ed537362e9",
+    "url": "/static/js/main.a7a4d82e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
